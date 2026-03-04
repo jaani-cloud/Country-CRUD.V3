dcc9d0d37ca566dae618c0b6c196595d703f4606
heredikshit-ux <jaanilyrical@gmail.com>
2026-03-04 18:05:54 +0530

    App - Services(Country)

---
+++ App/Services/Classes/CountryService.cs
@@ -0,0 +1,81 @@
+﻿using App.DTOs.Countries;
+using App.Services.Interfaces;
+using Domain.Entities;
+using Infra.Repos.Interfaces;
+
+namespace App.Services.Classes;
+
+public class CountryService : ICountryService
+{
+    private readonly ICountryRepo _countryRepo;
+
+    public CountryService(ICountryRepo countryRepo)
+    {
+        _countryRepo = countryRepo;
+    }
+
+    public async Task Add(CountryCreateUpdateDto input)
+    {
+        Country country = new Country
+        {
+            Name = input.Name.ToLower(),
+        };
+        await _countryRepo.Add(country);
+    }
+
+    public async Task Update(CountryCreateUpdateDto input, int id)
+    {
+        var country = await _countryRepo.GetById(id);
+
+        if (country == null) return;
+
+        country.Name = input.Name.ToLower();
+
+        await _countryRepo.Update(country);
+    }
+
+    public async Task Delete(int id)
+    {
+        var country = await _countryRepo.GetById(id);
+        if (country == null) return;
+
+        await _countryRepo.Delete(country);
+    }
+
+    public async Task<CountryResponseDto?> GetByName(string name)
+    {
+        var country = await _countryRepo.GetByName(name);
+
+        if (country == null) return null;
+
+        return new CountryResponseDto
+        {
+            Id = country.Id,
+            Name = country.Name,
+        };
+    }
+
+    public async Task<CountryResponseDto?> GetById(int id)
+    {
+        var country = await _countryRepo.GetById(id);
+
+        if (country == null) return null;
+
+        return new CountryResponseDto
+        {
+            Id = country.Id,
+            Name = country.Name,
+        };
+    }
+
+    public async Task<List<CountryResponseDto>> GetAll()
+    {
+        var countries = await _countryRepo.GetAll();
+
+        return countries.Select(c => new CountryResponseDto
+        {
+            Id = c.Id,
+            Name = c.Name,
+        }).ToList();
+    }
+}
---
+++ App/Services/Interfaces/ICountryService.cs
@@ -0,0 +1,13 @@
+﻿using App.DTOs.Countries;
+
+namespace App.Services.Interfaces;
+
+public interface ICountryService
+{
+    public Task Add(CountryCreateUpdateDto input);
+    public Task Update(CountryCreateUpdateDto input, int id);
+    public Task Delete(int id);
+    public Task<CountryResponseDto?> GetByName(string name);
+    public Task<CountryResponseDto?> GetById(int id);
+    public Task<List<CountryResponseDto>> GetAll();
+}
