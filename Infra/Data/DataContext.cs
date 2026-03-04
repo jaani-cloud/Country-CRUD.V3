20ff40a8e0c60081194ea2bbffde2dd48194e195
heredikshit-ux <jaanilyrical@gmail.com>
2026-03-04 11:59:06 +0530

    Infra - Data(DataContext)

---
+++ Infra/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<State> States { get; set; }
+}
