26bb6b26437c32243413ddb879f17ef4b5398faa
heredikshit-ux <jaanilyrical@gmail.com>
2026-03-04 23:04:39 +0530

    Infra - Repos(City)

---
+++ Infra/Repos/Classes/CityRepo.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Infra.Data;
+using Infra.Repos.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repos.Classes;
+
+public class CityRepo : ICityRepo
+{
+    private readonly DataContext _context;
+    public CityRepo(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Add(City city)
+    {
+        _context.Cities.Add(city);
+        await _context.SaveChangesAsync();
+    }
+    public async Task Update(City city)
+    {
+        _context.Cities.Update(city);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(City city)
+    {
+        _context.Cities.Remove(city);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<City?> GetByName(string name) =>
+        await _context.Cities.AsNoTracking().Include(c => c.State).FirstOrDefaultAsync(c => c.Name == name);
+
+    public async Task<City?> GetBYId(int id) => await _context.Cities.Include(c => c.State).FirstOrDefaultAsync(c => c.Id == id);
+    public async Task<List<City>> GetAll() => await _context.Cities.AsNoTracking().Include(c => c.State).ToListAsync();
+}
---
+++ Infra/Repos/Interfaces/ICityRepo.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Infra.Repos.Interfaces;
+
+public interface ICityRepo
+{
+    public Task Add(City city);
+    public Task Update(City city);
+    public Task Delete(City city);
+    public Task<City?> GetByName(string name);
+    public Task<City?> GetBYId(int id);
+    public Task<List<City>> GetAll();
+}
