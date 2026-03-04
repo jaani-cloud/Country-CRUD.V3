19d754dc1218377804eb622693c8a05aee2cb3a5
heredikshit-ux <jaanilyrical@gmail.com>
2026-03-04 17:23:14 +0530

    App - DTOs(Country, State)

---
+++ App/DTOs/States/StateResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using App.DTOs.Countries;
+using Infra.Repos.Classes;
+
+namespace App.DTOs.States;
+
+public class StateResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public CountryResponseDto? Country { get; set; }
+}
