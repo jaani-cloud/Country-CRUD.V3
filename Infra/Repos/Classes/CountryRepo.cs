4cfe3f54fb8226e446d05bc0d905d9ec85f4f5bf
heredikshit-ux <jaanilyrical@gmail.com>
2026-03-04 16:18:20 +0530

    Infra - Repos(Country)

---
+++ Infra/Repos/Classes/CountryRepo.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using Infra.Data;
+using Infra.Repos.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repos.Classes;
+
+public class CountryRepo : ICountryRepo
+{
+    private readonly DataContext _context;
+
+    public CountryRepo(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Add(Country country)
+    {
+        _context.Countries.Add(country);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(Country country)
+    {
+        _context.Entry(country).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(Country country)
+    {
+        _context.Countries.Remove(country);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Country?> GetByName(string name) => await _context.Countries.FirstOrDefaultAsync(c => c.Name == name);
+
+    public async Task<Country?> GetById(int id) => await _context.Countries.FindAsync(id);
+
+    public async Task<List<Country>> GetAll() => await _context.Countries.AsNoTracking().ToListAsync();
+
+}
---
+++ Infra/Repos/Interfaces/ICountryRepo.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Infra.Repos.Interfaces;
+
+public interface ICountryRepo
+{
+    public Task Add(Country country);
+    public Task Update(Country country);
+    public Task Delete(Country country);
+    public Task<Country?> GetByName(string name);
+    public Task<Country?> GetById(int id);
+    public Task<List<Country>> GetAll();
+}
